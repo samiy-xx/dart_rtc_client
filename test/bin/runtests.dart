9aef4efd2a2dc81769464b68a6f6e55e89b487af
Sami Ylonen <sami.ylonen@gmail.com>
2013-02-25 21:45:27 +0100

    support for drone.io someday

---
+++ test/bin/runtests.dart
@@ -0,0 +1,7 @@
+import 'dart:html';
+import '../rtc_client_tests.dart' as test;
+
+void main() {
+  test.run();
+}
+
--- test/rtc_client_tests.dart
+++ test/rtc_client_tests.dart
@@ -3,6 +3,7 @@ library rtc_client_tests;
 import 'dart:html';
 import '../packages/unittest/unittest.dart';
 import '../packages/unittest/html_enhanced_config.dart';
+import '../packages/unittest/html_config.dart';
 import '../packages/unittest/mock.dart';
 import '../lib/rtc_client.dart';
 import 'package:dart_rtc_common/rtc_common.dart';
@@ -15,8 +16,8 @@ part 'tests/binarywritertests.dart';
 part 'tests/binaryreadertests.dart';
 
 void run() {
-  useHtmlEnhancedConfiguration();
-  
+  //useHtmlEnhancedConfiguration();
+  useHtmlConfiguration();
   new PacketTests().run();
   new BinaryWriterTests().run();
   new BinaryReaderTests().run();

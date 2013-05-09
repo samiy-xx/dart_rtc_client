7770c369576a67745da3878a0dfe3575c1c55b56
Sami Ylonen <sami.ylonen@gmail.com>
2013-05-09 18:41:32 +0200

    massive speed bump after removing traces of being retarded

--- lib/src/peer/udpdatawriter.dart
+++ lib/src/peer/udpdatawriter.dart
@@ -1,6 +1,105 @@
 part of rtc_client;
 
+class SendItem {
+  ByteBuffer buffer;
+  int signature;
+  int sequence;
+
+  SendItem(this.buffer, this.sequence, this.signature);
+}
 class UDPDataWriter extends BinaryDataWriter {
+  List<SendItem> _sentItems;
+  Completer _completer;
+  Timer _intervalTimer = null;
+  int _startSend;
+
+  UDPDataWriter(PeerWrapper wrapper) : super(BINARY_PROTOCOL_UDP, wrapper) {
+    _sentItems = new List<SendItem>();
+  }
+
+  Future<int> send(ByteBuffer buffer, int packetType, bool reliable) {
+    _startSend = new DateTime.now().millisecondsSinceEpoch;
+    Completer completer = new Completer();
+    if (!reliable)
+      completer.complete(0);
+
+    int totalSequences = (buffer.lengthInBytes ~/ _writeChunkSize) + 1;
+    int sequence = 1;
+    int read = 0;
+    int leftToRead = buffer.lengthInBytes;
+    int signature = new Random().nextInt(100000000);
+    _completer = completer;
+
+    while (read < buffer.lengthInBytes) {
+      int toRead = leftToRead > _writeChunkSize ? _writeChunkSize : leftToRead;
+      ByteBuffer toAdd = new Uint8List.fromList(new Uint8List.view(buffer).sublist(read, read+toRead));
+      ByteBuffer b = addUdpHeader(
+          toAdd,
+          packetType,
+          sequence,
+          totalSequences,
+          signature,
+          buffer.lengthInBytes
+      );
+      _signalWriteChunk(signature, sequence, totalSequences, toAdd.lengthInBytes);
+      write(b);
+      _signalWroteChunk(signature, sequence, totalSequences, toAdd.lengthInBytes);
+      _sentItems.add(new SendItem(b, sequence, signature));
+      sequence++;
+      read += toRead;
+      leftToRead -= toRead;
+    }
+    _setImmediate();
+    return completer.future;
+  }
+
+  void _process() {
+    if (_sentItems.length == 0)
+      _completer.complete(new DateTime.now().millisecondsSinceEpoch - _startSend);
+    else
+      _setImmediate();
+  }
+
+  void writeAck(int signature, int sequence, int total) {
+    new Timer(const Duration(milliseconds: 0), () {
+      write(BinaryData.createAck(signature, sequence));
+    });
+  }
+
+  void receiveAck(int signature, int sequence) {
+    new Timer(const Duration(milliseconds: 0), () {
+      _sentItems.removeWhere((SendItem i) => i.signature == signature && i.sequence == sequence);
+
+      if (_sentItems.length == 0)
+        print("sentItems empty");
+    });
+  }
+
+  void _signalWriteChunk(int signature, int sequence, int totalSequences, int bytes) {
+    new Timer(const Duration(milliseconds: 0), () {
+      listeners.where((l) => l is BinaryDataSentEventListener).forEach((BinaryDataSentEventListener l) {
+        l.onWriteChunk(_wrapper, signature, sequence, totalSequences, bytes);
+      });
+    });
+  }
+
+  void _signalWroteChunk(int signature, int sequence, int totalSequences, int bytes) {
+    new Timer(const Duration(milliseconds: 0), () {
+      listeners.where((l) => l is BinaryDataSentEventListener).forEach((BinaryDataSentEventListener l) {
+        l.onWroteChunk(_wrapper, signature, sequence, totalSequences, bytes);
+      });
+    });
+  }
+
+  void _setImmediate() {
+    if (_intervalTimer != null)
+      _intervalTimer.cancel();
+    _intervalTimer = new Timer(const Duration(milliseconds: 5), _process);
+  }
+}
+
+
+class UDPDataWriterOld extends BinaryDataWriter {
   Sequencer _sequencer;
   bool _canLoop = true;
   int _last;
@@ -9,7 +108,7 @@ class UDPDataWriter extends BinaryDataWriter {
   int _start = 0;
   int loops;
   int sent;
-  UDPDataWriter(PeerWrapper wrapper) : super(BINARY_PROTOCOL_UDP, wrapper) {
+  UDPDataWriterOld(PeerWrapper wrapper) : super(BINARY_PROTOCOL_UDP, wrapper) {
     _sequencer = new Sequencer();
     _last = new DateTime.now().millisecondsSinceEpoch;
   }
@@ -42,7 +141,8 @@ class UDPDataWriter extends BinaryDataWriter {
           signature,
           buffer.lengthInBytes
       );
-      addSequence(signature, sequence, totalSequences, b, reliable);
+      //addSequence(signature, sequence, totalSequences, b, reliable);
+      write(b);
       sequence++;
       read += toRead;
       leftToRead -= toRead;
